abeb93ea0a41bcb5808f3efa1766393ba02d13a0
Ezejanu <Ezejanu_nno@ymail.com>
2024-03-29 02:10:07 +0000

    task 5

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+from flask import Flask, jsonify
+
+app = Flask(__name_-)
+
+
+@app.route("/")
+def index():
+    """
+    Route handler for the root endpoint.
+
+    Returns:
+        dict: A JSON payload with a welcome message.
+    """
+
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    """
+    This block of code runs the Flask app when the script is executed directly.
+    """
+    app.run(host="0.0.0.0", port="5000")
